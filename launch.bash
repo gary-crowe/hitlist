571be61dbe014cb77192763cd202e408e0841937
Gary Crowe <gary.crowe@computacenter.com>
2023-05-15 13:39:19 +0100

    Playing

--- launch.bash
+++ launch.bash
@@ -1,13 +1,12 @@
 # Use this to pull from public repo using https:
-#oc new-app https://github.com/gary-crowe/hitlist \
-#	--context-dir=flask-with-oshift \
-#	--source-secret repo-at-github \
-#	--labels app.kubernetes.io/name=python,app.kubernetes.io/part-of=thehitlist \
-#	--name theblacklist \
-#	-e MYSQL_HOST=mysql \
-#	-e MYSQL_DATABASE=offenders \
-#	-e MYSQL_USER=gary \
-#	-e MYSQL_PASSWORD=redhat123
+oc new-app https://github.com/gary-crowe/hitlist \
+	--context-dir=flask-with-oshift \
+	--labels app.kubernetes.io/name=python,app.kubernetes.io/part-of=thehitlist \
+	--name theblacklist \
+	-e MYSQL_HOST=mysql \
+	-e MYSQL_DATABASE=offenders \
+	-e MYSQL_USER=gary \
+	-e MYSQL_PASSWORD=redhat123
 
 # NB. If you launch this from a differtnt namespace, target the actual namesapce like this:
 # -e MYSQL_HOST=mysql.blacklist
@@ -15,12 +14,12 @@
 # Use this is you have created a private key and secret
 #See: create_github_secret.bash in this folder
 
-oc new-app python~git@github.com:gary-crowe/hitlist.git \
-	--context-dir=flask-with-oshift \
-	--source-secret repo-at-github \
-	--name theblacklist \
-	--labels app.kubernetes.io/name=python,app.kubernetes.io/part-of=thehitlist \
-	-e MYSQL_HOST=mysql \
-	-e MYSQL_DATABASE=offenders \
-	-e MYSQL_USER=gary \
-	-e MYSQL_PASSWORD=redhat123
+#oc new-app python~git@github.com:gary-crowe/hitlist.git \
+#	--context-dir=flask-with-oshift \
+#	--source-secret repo-at-github \
+#	--name theblacklist \
+#	--labels app.kubernetes.io/name=python,app.kubernetes.io/part-of=thehitlist \
+#	-e MYSQL_HOST=mysql \
+#	-e MYSQL_DATABASE=offenders \
+#	-e MYSQL_USER=gary \
+#	-e MYSQL_PASSWORD=redhat123
--- launch_mysql.bash
+++ launch_mysql.bash
@@ -1,19 +1,18 @@
 # Use this to pull direct if it's a public repo. http protocol
-#oc new-app mysql:8.0~https://github.com/gary-crowe/hitlist \
-#	--name mysql \
-#	--source-secret repo-at-github \
-#	--context-dir=flask-with-oshift \
-#	--labels app.kubernetes.io/name=mysql-database,app.kubernetes.io/part-of=thehitlist \
-#	--env MYSQL_DATABASE=offenders \
-#	--env MYSQL_USER=gary \
-#	--env MYSQL_PASSWORD=redhat123
-
-# Use this if you have setup a private key and added as a secret.
-oc new-app mysql:8.0~git@github.com:gary-crowe/hitlist.git \
-	--name mysql  \
-	--source-secret repo-at-github \
+oc new-app mysql:8.0~https://github.com/gary-crowe/hitlist \
+	--name mysql \
 	--context-dir=flask-with-oshift \
-	--labels app.kubernetes.io/name=mysql-database,app.kubernetes.io/part-of=thehitlist,app=mysql \
+	--labels app.kubernetes.io/name=mysql-database,app.kubernetes.io/part-of=thehitlist \
 	--env MYSQL_DATABASE=offenders \
 	--env MYSQL_USER=gary \
 	--env MYSQL_PASSWORD=redhat123
+
+# Use this if you have setup a private key and added as a secret.
+#oc new-app mysql:8.0~git@github.com:gary-crowe/hitlist.git \
+#	--name mysql  \
+#	--source-secret repo-at-github \
+#	--context-dir=flask-with-oshift \
+#	--labels app.kubernetes.io/name=mysql-database,app.kubernetes.io/part-of=thehitlist,app=mysql \
+#	--env MYSQL_DATABASE=offenders \
+#	--env MYSQL_USER=gary \
+#	--env MYSQL_PASSWORD=redhat123
